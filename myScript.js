2482b86e58d35a862062c711851930d59fa1d0d4
AbhishekDhankhar <56167884+AbhishekDhankhar@users.noreply.github.com>
2020-07-07 23:34:14 +0530

    update

---
+++ myScript.js
@@ -0,0 +1,9 @@
+ //sidenav
+function openNav() {
+ document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+//sidenaive
